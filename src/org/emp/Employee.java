7db629294b04201df613bc49cc76cd6a39243836
mdhakkim1596 <mdhakkim1596@gmail.com>
2022-05-24 17:34:34 +0530

    new code

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,73 @@
+package org.emp;
+
+public class Employee {
+	
+	public static void main(String[] args) {
+	
+		byte age = 25;
+		System.out.println(age);
+		System.out.println("age");
+		
+		short year = 2022;
+		System.out.println(year);
+		System.out.println("current year is " + year);
+		
+		int i = 987654321;
+		System.out.println(i);
+		System.out.println(i + " 100");
+		System.out.println(i+100);
+		
+		long l = 9876543210l;
+		System.out.println(l);
+		System.out.println(l + " 100");
+		System.out.println(l+100);
+		
+		float f = 54387.87f;
+		System.out.println(f);
+		System.out.println(f + " 100");
+		System.out.println(f+100);
+		
+		float fl = 20/9f;
+		System.out.println(fl);
+		System.out.println(fl + " 100");
+		System.out.println(fl+100);
+		
+		double d = 479/7d;
+		System.out.println(d);
+		System.out.println(d + " 100");
+		System.out.println(d+100);
+		
+		char g = 'M';
+		System.out.println(g);
+		
+		char ge = 'F';
+		System.out.println(ge);
+		
+		boolean b = false;
+		System.out.println(b);
+		
+		boolean bo = true;
+		System.out.println(bo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
